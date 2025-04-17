8e10402116d80403dbf6c0276cece22e76285bb8
TheFilimisco <54910978@iespoblenou.org>
2025-04-17 10:59:43 +0200

    little fix bug with exceptions

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,30 @@
 import ExternalDragDrop from './ExternalDragDrop';
 
 export default async function SchedulePage() {
-  const [employeesRes, workSpacesRes, dateRegistersRes] = await Promise.all([
-    fetch('http://localhost:5000/api/employees', { cache: 'no-store' }),
-    fetch('http://localhost:5000/api/work_spaces', { cache: 'no-store' }),
-    fetch('http://localhost:5000/api/date_registers', { cache: 'no-store' }),
-  ]);
+  try{
+    const [employeesRes, workSpacesRes, dateRegistersRes] = await Promise.all([
+      fetch('http://localhost:5000/api/employees', { cache: 'no-store' }),
+      fetch('http://localhost:5000/api/work_spaces', { cache: 'no-store' }),
+      fetch('http://localhost:5000/api/date_registers', { cache: 'no-store' }),
+    ]);
+  
+    const [employees, workSpaces, dateRegisters] = await Promise.all([
+      employeesRes.json(),
+      workSpacesRes.json(),
+      dateRegistersRes.json(),
+    ]);
+  
+    return (
+      <ExternalDragDrop
+        employees={employees}
+        workSpaces={workSpaces}
+        dateRegisters={dateRegisters}
+      />
+    );
+  } catch (error){
+     console.error('Error to get Data:', error);
+  }
 
-  const [employees, workSpaces, dateRegisters] = await Promise.all([
-    employeesRes.json(),
-    workSpacesRes.json(),
-    dateRegistersRes.json(),
-  ]);
+  }
 
-  return (
-    <ExternalDragDrop
-      employees={employees}
-      workSpaces={workSpaces}
-      dateRegisters={dateRegisters}
-    />
-  );
-}
+
