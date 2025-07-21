4cc70a6f542ab6f2e0d91bb3d78a36f794b34d92
TheFilimisco <54910978l@iespoblenou.org>
2025-07-21 17:41:19 +0200

    fix page on department

--- src/app/dashboard/department/page.js
+++ src/app/dashboard/department/page.js
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import DepartmentForm from "./department-form";
 
 export default function Home() {
+  S;
   return (
     <main className="!min-h-screen">
       <Suspense fallback={<div>Loading form...</div>}>
